f803cb701e5c6af6223d7f3d23b382180e52482a
Leo <uchihaitachihtun@gmail.com>
2016-09-08 17:01:51 +0630

    Delete todo with id

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var _= require('underscore');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -20,8 +20,8 @@ app.get('/todos', function(req, res) {
 //GET/todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos,{id: todoId});
-    
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+
     if (matchedTodo) {
         res.json(matchedTodo);
     } else {
@@ -29,18 +29,34 @@ app.get('/todos/:id', function(req, res) {
     }
 });
 
+//POST /todos
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, "description", 'completed');
 
-    if(! _.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-    	return res.status(400).send();
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
     }
     //add id field
+    body.description = body.description.trim();
     body.id = todoNextId++;
 
     //push body into array
     todos.push(body);
     res.json(body);
+});
+
+// Delete /todo/
+
+app.delete("/todos/:id", function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+
+    if (!matchedTodo) {
+        res.status(404).json({ "error": "no todos found with that id" });
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
 });
 
 app.listen(PORT, function() {
